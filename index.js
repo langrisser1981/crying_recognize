06fdadb956b40b5c08249ea889aa68aa53715cda
Cheng. Lenny (TPE) <Lenny_Cheng@compal.com>
2019-09-06 18:40:09 +0800

    batch recognize completed

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ const cid =  '7018883e11b96b5b265b092f16daf5ea'
 
 let token;
 let config;
+const raw_type = ['Wet diaper', 'Hungry', 'Unwell', 'Sleepy', 'Unknown'];
+const _type = ['Diper', 'Hungry', 'Pain', 'Sleepy', 'Unknown'];
 
-const getToken = async () => {
+const getToken = async ()=>{
 	try {
 		let url = `http://${baseUrl}company/${cid}/token`;
 		let response = await axios.get(url);
@@ -53,87 +55,98 @@ const recognize = async (email, filePath) => {
 		result = data.recognizeResult.split('@');
 		label = result[0];
 		coef = result[1].substr(1, result[1].length-2).split(' ').map(item=>{
-			let n = parseInt(item);
+			let n = parseFloat(item);
 			return n;
 		});
 
 		str = `標籤:${label}, 百分比:${coef}, 原始回傳:${result}, 檔名:${filePath}`
-		console.log(str);
+		//console.log(str);
+		let res = [label, coef];
+		return Promise.resolve(res);
 
-		return str;
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-const readCSV = ()=>{
-	let csvData=[];
-	fs.createReadStream('data.csv')
-		.pipe(parse({delimiter: ','}))
-		.on('data', function(csvrow) {
-			//console.log(csvrow);
-			//do something with csvrow
+const readCSV = async ()=>{
+	return new Promise((resolve, reject)=>{
+		let csvData=[];
+		fs.createReadStream('data.csv')
+			.pipe(parse({delimiter: ','}))
+			.on('data', function(csvrow) {
+				//console.log(csvrow);
+				//do something with csvrow
 
-			const raw_type = ['Unwell', 'Hungry', 'Unwell', 'Sleepy', 'Unknown'];
-			const _type = ['Diper', 'Hungry', 'Pain', 'Sleepy', 'Unknown'];
-			let id = `${csvrow[0]}@compal.com`;
-			let type = _type[raw_type.indexOf(csvrow[2])];
+				let id = `${csvrow[0]}@compal.com`;
+				let type = _type[raw_type.indexOf(csvrow[2])];
+				let userAns = _type[raw_type.indexOf(csvrow[3])];
 
-			let day = csvrow[6];
-			let time = csvrow[7];
-			let mm = day.substr(0,2);
-			let dd = day.substr(2,2);
-			let hh = time.substr(0,2);
-			let MM = time.substr(2,2);
-			let ss = time.substr(4,2);
-			let fileName = `cry_2019_${mm}_${dd}_${hh}_${MM}_${ss}.wav`
+				let day = csvrow[6];
+				let time = csvrow[7];
+				let mm = day.substr(0,2);
+				let dd = day.substr(2,2);
+				let hh = time.substr(0,2);
+				let MM = time.substr(2,2);
+				let ss = time.substr(4,2);
+				let fileName = `cry_2019_${mm}_${dd}_${hh}_${MM}_${ss}.wav`
 
-			if(id && type && fileName){
-				let directoryPath = path.join(__dirname, id, type, fileName);
-				//console.log(directoryPath);
-				let email = id;
-				let src = directoryPath;
-				csvData.push({
-					email:email,
-					src:src
-				});        
-			}
-		})
-		.on('end',function() {
-			//do something with csvData
-			const fields = ['email', 'src'];
-			const csv = json2csv.parse(csvData, fields);
-			fs.writeFile('clearData.csv', csv, (err)=>{
-				if(err) throw err;
-				//console.log(csvData);
-				console.log('file saved');
-			});
-			csvData.reduce(async(promise, item)=>{
-				let email = item.email;
-				let src = item.src;
-				console.log(`email:${email}, src:${src}`);
-				email = 'D3PA853WRF3MSG6GUH7J@compal.com'
-				src = 'C://Users/lenny_cheng/Desktop/git_test/crying_samples/D3PA853WRF3MSG6GUH7J@compal.com/Diper/cry_2019_07_17_08_40_56.wav';
-				return promise.then(()=>{
-					let res = recognize(email, src);
-					return res;
-
+				if(id && type && fileName){
+					let directoryPath = path.join(__dirname, 'crying_samples', id, type, fileName);
+					//console.log(directoryPath);
+					let email = id;
+					let src = directoryPath;
+					csvData.push({
+						email:email,
+						src:src,
+						prevAns:type,
+						userAns:userAns,
+						newAns:''
+					});        
 				}
-				);
-				/*
-				*/
-			},Promise.resolve());
-
-		});
+			})
+			.on('end',function() {
+				//do something with csvData
+				return resolve(csvData);
+			});
+	});
 }
 
-
-getToken()
-	.then(res=>{
-		console.log(res);
-		readCSV();
-	})
-	.catch(err=>{
-		console.log(err);
+const writeCSV = async (data)=>{
+	const fields = ['email', 'src', 'prevAns', 'userAns', 'newAns'];
+	const csv = json2csv.parse(data, fields);
+	fs.writeFile('clearData.csv', csv, (err)=>{
+		if(err) throw err;
+		//console.log(csvData);
+		console.log('file saved');
 	});
+}
+
+(async function(){
+	let token = await getToken();
+	let csvData = await readCSV();
+	console.log(csvData);
+
+	csvData.reduce(async(promise, item)=>{
+		return promise.then(async()=>{
+			let email = item.email;
+			let src = item.src;
+			//console.log(`email:${email}, src:${src}`);
+			email = 'D3PA853WRF3MSG6GUH7J@compal.com'
+			try{
+				let res = await recognize(email, src);
+				console.log(`標籤:${res[0]}, 百分比:${res[1]}, 原始答案:${item.prevAns}, 使用者答案:${item.userAns}, 新答案:${_type[res[0]]}`)
+				item.newAns = res[0];
+			}catch(error){
+				console.log(error);
+			}
+
+			return Promise.resolve();
+
+		});
+	},Promise.resolve());
+	/*
+	*/
+	writeCSV(csvData);
+})()
 
